26b7b987f79bedf990fcb844f93b2948401f5cf2
Tiago Ratto <tiagoratto@gmail.com>
2017-03-21 03:12:33 +0000

    - Changed gulpfile version - Changed CSS and image filenames - Added Theme Switcher - Added EoE and FaD themes - Added two pages backgrounds

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var uglifycss = require('gulp-uglifycss');
 var concat = require('gulp-concat');
 var templates = require('gulp-angular-templatecache');
 var minifyHTML = require('gulp-htmlmin');
-var version = "0.9.4";
+var version = "0.9.5";
 
 gulp.task('templates', function () {
   gulp.src([
@@ -13,7 +13,7 @@ gulp.task('templates', function () {
     .pipe(minifyHTML({collapseWhitespace: true}))
     .pipe(templates('templates.js', {
       root: '/views/',
-      module: 'swaor'
+      module: 'swrpgwc'
    }))
     .pipe(gulp.dest('tmp'));
 });
@@ -28,16 +28,28 @@ gulp.task("images", function() {
 })
 
 gulp.task("styles", function() {
-   gulp.src(['src/styles/*.css'])
+   gulp.src(['src/styles/fonts.css', 'src/styles/printer.css'])
+      .pipe(concat('swrpg.min.css'))
+      .pipe(uglifycss())
+      .pipe(gulp.dest('releases/' + version + "/styles"));
+   gulp.src(['src/styles/eoe.css'])
+      .pipe(concat('swrpgeoe.min.css'))
+      .pipe(uglifycss())
+      .pipe(gulp.dest('releases/' + version + "/styles"));
+   gulp.src(['src/styles/aor.css'])
       .pipe(concat('swrpgaor.min.css'))
       .pipe(uglifycss())
-      .pipe(gulp.dest('releases/' + version + "/styles"))
+      .pipe(gulp.dest('releases/' + version + "/styles"));
+   gulp.src(['src/styles/fad.css'])
+      .pipe(concat('swrpgfad.min.css'))
+      .pipe(uglifycss())
+      .pipe(gulp.dest('releases/' + version + "/styles"));
 })
 
 gulp.task('release', ['fonts', 'images', 'styles','templates'], function() {
-    return gulp.src(['src/scripts/starship.js', 'src/scripts/dice.js', 'src/scripts/**/*.js', '!./src/pkg/*', 'tmp/*.js'])
-		  .pipe(concat('swrpgaor.min.js', {newLine: ';'}))
-        .pipe(uglify())
+    return gulp.src(['src/scripts/starship.js', 'src/scripts/dice.js', 'src/scripts/jdf-ngThemeSwitcher.min.js', 'src/scripts/**/*.js', '!./src/pkg/*', 'tmp/*.js'])
+		  .pipe(concat('swrpgwc.min.js', {newLine: ';'}))
+        //.pipe(uglify())
         .pipe(gulp.dest('releases/' + version + "/scripts"));
 });
 
---
+++ src/scripts/jdf-ngThemeSwitcher.js
@@ -0,0 +1,20 @@
+/*! 
+ * jdf-ngThemeSwitcher v0.1.0
+ * http://jdforsythe.github.io/jdf-ngThemeSwitcher
+ * Copyright (c) 2014 Jeremy Forsythe <jdforsythe@gmail.com>
+ * License: MIT
+ */
+(function() {
+	function ThemeSwitcherDirective($sce) {
+		return {
+			restrict: 'E',
+			scope: {
+				urls: '='
+			},
+			template: '<link rel="stylesheet" ng-href="{{url}}" ng-repeat="url in urls" />'
+		};
+	}
+
+	angular.module('jdf.ngThemeSwitcher', [])
+			.directive('themeSwitcher', ['$sce',ThemeSwitcherDirective]);
+})();
